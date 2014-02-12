15d6e61de0f520a9bc69fe122d4f59ebc48d60a5
Joseph Abrahamson <me@jspha.com>
2014-02-12 16:23:47 -0500

    Better Protocol definition

--- src/Test/Cuke.hs
+++ src/Test/Cuke.hs
@@ -6,6 +6,8 @@ import           Control.Applicative
 import qualified Control.Concurrent         as Conc
 import qualified Control.Concurrent.Async   as Async
 import           Control.Monad
+import           Control.Monad.Identity
+import           Control.Monad.Morph
 import           Control.Monad.State.Strict
 import qualified Data.Aeson                 as Ae
 import qualified Data.Aeson.Types           as Ae
@@ -13,115 +15,61 @@ import qualified Data.Attoparsec            as Atto
 import qualified Data.ByteString            as S
 import qualified Data.ByteString.Lazy       as Sl
 import qualified Data.Text                  as T
+import           Lens.Family
 import           Pipes
-import qualified Pipes.Attoparsec           as Pa
+import qualified Pipes.ByteString           as Ps
+import           Pipes.Core
+import           Pipes.Group
 import           Pipes.Network.TCP
 import qualified Pipes.Prelude              as P
+import           Test.Cuke.Protocol
 
 fastMain :: Int -> IO ()
 fastMain n = void $ Async.race (Conc.threadDelay (n * 1000000)) main
 
+parseGroups :: Monad m => Atto.Parser a -> FreeT (Producer S.ByteString m) m r -> Producer (Either String a) m r
+parseGroups p = folds go (Atto.parse p S.empty) end where
+  go  f@Atto.Fail{} _      = f
+  go  d@Atto.Done{} _      = d
+  go  (Atto.Partial f) a   = f a
+  end (Atto.Fail lf ctx e) = Left e
+  end Atto.Partial{}       = Left "incomplete"
+  end (Atto.Done _ a)      = Right a
+
+wireIn :: MonadIO m => Socket -> Producer (Either String WireMessage) m ()
+wireIn s = parseGroups Ae.json' (fromSocket s 4096 ^. Ps.lines) >-> P.map decoder where
+  decoder eit = do
+    val <- eit
+    Ae.parseEither Ae.parseJSON val
+
+wireOut :: MonadIO m => Socket -> Consumer WireResponse m ()
+wireOut s = printer >-> toSocket s where
+  printer = forever $ do
+    resp <- await
+    Ps.fromLazy (Ae.encode resp)
+    yield "\n"
+
+midIO :: Pipe WireMessage WireResponse IO r
+midIO = hoist (return . runIdentity) mid
+
+mid :: Pipe WireMessage WireResponse Identity r
+mid = P.map go where
+  go :: WireMessage -> WireResponse
+  go = undefined
+  -- go (StepQuery step)    = Success (Just [SuccessArg "1" [] Nothing Nothing])
+  -- go (Invoke id args)    = Failure (Just $ FailureMsg (Just "1") ["2", "3", "4"] ["line 1", "line 2"])
+  -- go (BeginScenario _)   = Success Nothing
+  -- go (EndScenario _)     = Success Nothing
+  -- go (SnippetText _ _ _) = SuccessSnippet "whatever"
+
+middle :: Pipe (Either String WireMessage) WireResponse IO ()
+middle = dropLeft >-> midIO where
+  dropLeft = forever $ do
+    ei <- await
+    case ei of
+      Left  _ -> return ()
+      Right a -> yield a
+
 main :: IO ()
-main = serve (Host "localhost") "3901" $ \(sock, addr) -> do
-  res <- runEffect ( Pa.parsed Ae.json' (fromSocket sock 4096)
-                     >-> trans
-                     >-> toSocket sock
-                   )
-  case res of
-    Right r            -> return r
-    Left (pe, _resume) -> print pe
-
-trans :: Pipe Ae.Value S.ByteString IO r
-trans = 
-  decodePipe >-> dropLeft
-  >-> handle
-  >-> P.map (Sl.toStrict . Ae.encode)
-
-data WireMessage
-  = StepMatches T.Text
-  | Invoke T.Text [Ae.Value]
-  | BeginScenario [T.Text]
-  | EndScenario [T.Text]
-  | SnippetText { stepKeyword :: T.Text
-                , multilineArgClass :: T.Text
-                , stepName :: T.Text
-                }
-  | DiffFailed
-  | DiffOk
-  deriving ( Eq, Show )
-
-instance Ae.FromJSON WireMessage where
-  parseJSON v = do
-    packet <- Ae.parseJSON v
-    case packet of
-      [] -> fail "empty wire message tuple"
-      ["step_matches", obj] ->
-        flip (Ae.withObject "step_matches params") obj $ \o ->
-          StepMatches <$> o Ae..: "name_to_match"
-
-      ["invoke", obj] ->
-        flip (Ae.withObject "invoke params") obj $ \o ->
-          Invoke <$> o Ae..: "id"
-                 <*> o Ae..: "args"
-
-      ["begin_scenario"] -> pure (BeginScenario [])
-      ["begin_scenario", obj] ->
-        flip (Ae.withObject "begin_scenario params") obj $ \o ->
-          BeginScenario <$> o Ae..: "tags"
-      ["end_scenario"]   -> pure (EndScenario   [])
-      ["end_scenario", obj] ->
-        flip (Ae.withObject "end_scenario params") obj $ \o ->
-          BeginScenario <$> o Ae..: "tags"
-
-      ["snippet", obj] -> 
-        flip (Ae.withObject "snippet params") obj $ \o ->
-          SnippetText <$> o Ae..: "step_keyword"
-                      <*> o Ae..: "multiline_arg_class"
-                      <*> o Ae..: "step_name"
-
-      ["diff_failed"] -> pure DiffFailed
-      ["diff_ok"]     -> pure DiffOk
-
-data WireResponse
-  = Pending
-  | Failure FailureMsg
-  | Yikes
-  | Success (Maybe [SuccessArg])
-  | Diff DiffTable
-  | ImmediateDiff DiffTable
-  deriving ( Eq, Show )
-
-data DiffTable = DiffTable
-  deriving ( Eq, Show )
-
-data SuccessArg
-  = SuccessArg { sargId     :: T.Text
-               , sargArgs   :: [Ae.Value]
-               , sargSource :: T.Text
-               , sargRegexp :: T.Text
-               }
-  deriving ( Eq, Show )
-
-data FailureMsg
-  = FailureMsg { fmsgMessage   :: T.Text
-               , fmsgException :: Maybe T.Text
-               , fmsgBacktrace :: Maybe T.Text
-               }
-  deriving ( Eq, Show )
-        
-handle :: Monad m => Pipe Ae.Value Ae.Value m r
-handle = cat
-
-decodePipe
-  :: (Monad m, Ae.FromJSON a)
-  => Pipe Ae.Value (Either (String, Ae.Value) a) m r
-decodePipe = forever $ await >>= \v ->
-  case Ae.parse Ae.parseJSON v of
-    Ae.Success a -> yield (Right a)
-    Ae.Error   e -> yield (Left (e, v))
-
-dropLeft :: Monad m => Pipe (Either a b) b m r
-dropLeft = forever $ await >>= \eit ->
-  case eit of
-    Left  _ -> return ()
-    Right a -> yield a
+main = serve (Host "localhost") "3901" $ \(sock, addr) ->
+  runEffect ( wireIn sock >-> middle >-> wireOut sock )
---
+++ src/Test/Cuke/Protocol.hs
@@ -0,0 +1,165 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Cuke.Protocol where
+
+import           Control.Applicative
+import           Data.Aeson          ((.:), (.=))
+import qualified Data.Aeson          as Ae
+import           Data.Maybe
+import qualified Data.Text           as T
+
+data WireMessage
+  = StepQuery T.Text
+  | Invoke T.Text [Ae.Value]
+  | BeginScenario [T.Text]
+  | EndScenario [T.Text]
+  | DiffFailed
+  | DiffOk
+  | SnippetText { stepKeyword       :: T.Text
+                , multilineArgClass :: T.Text
+                , stepName          :: T.Text
+                }
+  deriving ( Eq, Show )
+
+instance Ae.FromJSON WireMessage where
+  parseJSON v = do
+    packet <- Ae.parseJSON v
+    case packet of
+      [] -> fail "empty wire message tuple"
+      ["step_matches", obj] ->
+        flip (Ae.withObject "step_matches params") obj $ \o ->
+          StepQuery <$> o .: "name_to_match"
+
+      ["invoke", obj] ->
+        flip (Ae.withObject "invoke params") obj $ \o ->
+          Invoke <$> o .: "id"
+                 <*> o .: "args"
+
+      ["begin_scenario"] -> pure (BeginScenario [])
+      ["begin_scenario", obj] ->
+        flip (Ae.withObject "begin_scenario params") obj $ \o ->
+          BeginScenario <$> o .: "tags"
+      ["end_scenario"]   -> pure (EndScenario   [])
+      ["end_scenario", obj] ->
+        flip (Ae.withObject "end_scenario params") obj $ \o ->
+          BeginScenario <$> o .: "tags"
+
+      ["snippet", obj] ->
+        flip (Ae.withObject "snippet params") obj $ \o ->
+          SnippetText <$> o .: "step_keyword"
+                      <*> o .: "multiline_arg_class"
+                      <*> o .: "step_name"
+
+      ["diff_failed"] -> pure DiffFailed
+      ["diff_ok"]     -> pure DiffOk
+
+data WireResponse
+  = MatchResponse      MatchResponse
+  | InvocationResponse InvocationResponse
+  | StructuralResponse StructuralResponse
+  | SnippetResponse    ProvideSnippet
+
+data MatchResponse
+  = StepMatches [StepArg]
+  | StepDoesNotMatch
+
+data StepArg
+  = StepArg { identifier :: T.Text
+            , arguments  :: [Ae.Value]
+            , stepSource :: Maybe T.Text
+            , stepRegexp :: Maybe T.Text
+            }
+
+data InvocationResponse
+  = InvocationPending   { reason :: T.Text }
+  | InvocationSucceeded
+  | DiffRequested       { recoverable :: Bool
+                        -- ^ If a diff is recoverable then we'll expect the
+                        -- interaction to continue. Otherwise, a failed
+                        -- diff will terminate the entire interaction.
+                        , diffTable   :: DiffTable
+                        }
+  | InvocationFailed FailureMsg
+
+data StructuralResponse
+  = ConfirmScenario
+  | ConfirmScenarioEnd
+
+data ProvideSnippet =
+  ProvideSnipet { snippet :: T.Text }
+
+data DiffTable = DiffTable
+  deriving ( Eq, Show )
+
+data FailureMsg
+  = FailureMsg { failMessage :: Maybe T.Text
+               -- ^ This should be a short description of the failure.
+               , exception   :: [T.Text]
+               -- ^ This ought to be a taxonomic path describing the
+               -- failure. When reported the segments will be intercalated
+               -- with separating dots.
+               , backtrace   :: [T.Text]
+               -- ^ This should report an organized list of failure
+               -- conditions. Normally it'd be a backtrace, but that's not
+               -- exactly what we get in Haskell.
+               }
+  deriving ( Eq, Show )
+
+(.=?) :: Ae.ToJSON a => T.Text -> Maybe a -> Maybe (T.Text, Ae.Value)
+_ .=? Nothing = Nothing
+t .=? Just a  = Just (t .= a)
+
+objectM :: [Maybe (T.Text, Ae.Value)] -> Ae.Value
+objectM = Ae.object . catMaybes
+
+success_ = Ae.String "success"
+fail_    = Ae.String "fail"
+pending_ = Ae.String "pending"
+diff_    = Ae.String "diff"
+diffi_   = Ae.String "diff!"
+
+instance Ae.ToJSON StepArg where
+  toJSON sa = objectM
+    [ Just ("id"     .=  identifier sa)
+    , Just ("args"   .=  arguments  sa)
+    ,       "source" .=? stepSource sa
+    ,       "regexp" .=? stepRegexp sa
+    ]
+
+instance Ae.ToJSON MatchResponse where
+  toJSON StepDoesNotMatch   = Ae.toJSON [fail_]
+  toJSON (StepMatches args) = Ae.toJSON [success_, Ae.toJSON args]
+
+instance Ae.ToJSON InvocationResponse where
+  toJSON (InvocationPending reason) = Ae.toJSON [pending_, Ae.toJSON reason]
+  toJSON InvocationSucceeded        = Ae.toJSON [success_]
+  toJSON (InvocationFailed fmsg)    = Ae.toJSON [fail_, Ae.toJSON fmsg]
+  toJSON d@DiffRequested{} 
+    | recoverable d = Ae.toJSON [ diff_,  Ae.toJSON (diffTable d) ]
+    | otherwise     = Ae.toJSON [ diffi_, Ae.toJSON (diffTable d) ]
+
+instance Ae.ToJSON StructuralResponse where
+  toJSON ConfirmScenario    = Ae.toJSON [success_]
+  toJSON ConfirmScenarioEnd = Ae.toJSON [success_]
+
+instance Ae.ToJSON ProvideSnippet where
+  toJSON ps = Ae.toJSON [success_, Ae.toJSON (snippet ps)]
+
+instance Ae.ToJSON FailureMsg where
+  toJSON fm = objectM
+    [ "message"   .=? failMessage fm
+    , "exception" .=? fmap (T.intercalate ".") (mayEmpty $ exception fm)
+    , "backtrace" .=? fmap T.unlines           (mayEmpty $ backtrace fm)
+    ]
+    where
+      mayEmpty [] = Nothing
+      mayEmpty xs = Just xs
+
+instance Ae.ToJSON DiffTable where
+  toJSON = undefined
+
+instance Ae.ToJSON WireResponse where
+  toJSON (MatchResponse mr)      = Ae.toJSON mr
+  toJSON (InvocationResponse ir) = Ae.toJSON ir
+  toJSON (StructuralResponse sr) = Ae.toJSON sr
+  toJSON (SnippetResponse ps)    = Ae.toJSON ps
